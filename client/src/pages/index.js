5f4bff04a1bdf23bbe2873ec4c619342373fff35
iqbal125 <iqbal125@yahoo.com>
2021-03-01 13:46:03 -0800

    rename directory

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Landing from '../screens/Marketing/LandingPage';
+import Landing from '../screens/Marketing/Landing';
 
 export default function Home() {
   return <Landing />;
--- client/src/pages/pricing.js
+++ client/src/pages/pricing.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Pricing from '../screens/Marketing/PricingPage';
+import Pricing from '../screens/Marketing/Pricing';
 import Layout from '../components/Marketing/Layout';
 
 export default function PricingPage() {
