fdd61ac07171d40cdc9945c93d7827528cdf6c1b
ioannaxen <ioannatxenaki@gmail.com>
2021-03-28 11:21:10 +0100

    Using new logo

--- client/src/components/Marketing/Navigation/header.js
+++ client/src/components/Marketing/Navigation/header.js
@@ -8,7 +8,7 @@ import FlyoutMenu from './flyoutMenu';
 
 import Button from '../../Common/buttons/PrimaryButton';
 import MobileMenu from './mobileMenu';
-import SmallLogo from '../../../assets/images/logo/small_logo.svg';
+import LargeLogo from '../../../components/Common/svgs/LargeLogo';
 import MenuImageSrc from '../../../assets/images/icons/menu.svg';
 import ChevronDown from '../../../assets/images/icons/chevron-down.svg';
 
@@ -21,16 +21,6 @@ const LogoWrapper = styled.div`
   @media (min-width: ${breakpoints.large}) {
     width: 0;
     flex: 1 1 0%;
-  }
-`;
-
-const Logo = styled.img`
-  cursor: pointer;
-  height: 2rem;
-  width: calc(2rem * 43 / 40);
-  @media (min-width: ${breakpoints.small}) {
-    height: 2.5rem;
-    width: calc(2.5rem * 43 / 40);
   }
 `;
 
@@ -182,7 +172,7 @@ const Header = () => {
       <Container2>
         <LogoWrapper>
           <Link to="/">
-            <Logo src={SmallLogo} alt="Logo" />
+            <LargeLogo />
           </Link>
         </LogoWrapper>
         <MenuWrapper ref={refMobile}>
--- client/src/components/Purchase/Navigation/altHeader.js
+++ client/src/components/Purchase/Navigation/altHeader.js
@@ -2,11 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 import { colors, breakpoints } from '../../../styles/theme';
 import { navigate } from 'gatsby';
+import SmallLogo from '../../Common/svgs/SmallLogo';
 
 const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: center;
   padding: 0.7rem;
   background-color: ${colors.coolGray50};
   @media (min-width: ${breakpoints.small}) {
@@ -15,20 +17,10 @@ const Wrapper = styled.div`
   }
 `;
 
-const Image = styled.img`
-  height: 3rem;
-  width: calc(3rem * 164 / 150);
-  cursor: pointer;
-`;
-
 const Header = () => {
   return (
     <Wrapper>
-      <Image
-        onClick={() => navigate('/')}
-        src="https://tailwindui.com/img/logos/v1/workflow-mark-on-white.svg"
-        alt="Workflow"
-      />
+      <SmallLogo height={50} width={59} onClick={() => navigate('/')} />
     </Wrapper>
   );
 };
